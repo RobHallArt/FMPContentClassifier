9bc846645fb45d3bec3f5345dd9626a49bc78f7d
Robert Hall <rob@csart.space>
2019-05-17 17:13:48 +0100

    Update button.cpp

--- src/button.cpp
+++ src/button.cpp
@@ -1,19 +1,24 @@
 #include "button.h"
 
+// Constructor function for buttons
 button::button(glm::vec2 _position, glm::vec2 _size, string _title) {
 	position = _position;
 	size = _size;
 	title = _title;
 }
 
+// Funtion for drawing buttons
 void button::draw(glm::vec2 _position, glm::vec2 _size) {
 
 	position = _position;
 	size = _size;
 
 		ofPushMatrix();
+		// Translate to button position
 		ofTranslate(position);
 		ofPushStyle();
+
+		// Set colour based on pressed state.
 		if (isPressed) {
 			ofSetColor(128);
 			
@@ -22,16 +27,19 @@ void button::draw(glm::vec2 _position, glm::vec2 _size) {
 			ofSetColor(255);
 		}
 		
+		// Draw outline rectangle
 		ofDrawRectangle(0, 0, size.x, size.y);
 		ofPopStyle();
 		ofPushStyle();
 		ofSetColor(0);
+		// Draw button title
 		ofDrawBitmapString(title, ofGetWidth()*0.02, ofGetWidth()*0.02);
 		ofPopStyle();
 		ofPopMatrix();
 
 }
 
+// Function to check if a button is pressed and return a boolean.
 bool button::mousePressed(int _x, int _y) {
 	if (_x > position.x && _x < (position.x + size.x) && _y > position.y && _y < (position.y + size.y)) {
 		isPressed = TRUE;
@@ -42,6 +50,7 @@ bool button::mousePressed(int _x, int _y) {
 	}
 }
 
+// Function to reset a button when it is released;
 void button::mouseReleased(int _x, int _y) {
 	if (_x > position.x && _x < (position.x + size.x) && _y > position.y && _y < (position.y + size.y)) {
 		isPressed = FALSE;
